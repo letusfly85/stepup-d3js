5d6ac7eb043edcd243c47d2059328592092f017f
letusfly85 <letusfly95@gmail.com>
2016-04-25 14:18:04 +0900

    variable load styles

---
+++ js/load_json.js
@@ -0,0 +1,20 @@
+d3.json("data/item1.json", function(error, data) {
+    var dataSet = [];
+    for (var i = 0; i < data.length; i++) {
+        dataSet.push(data[i].sales[0]);
+    }
+    d3.select("#myGraph")
+        .selectAll("rect")
+        .data(dataSet)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("width", function(d, i) {
+            return d;
+        })
+        .attr("height", "20")
+        .attr("x", "0")
+        .attr("y", function(d, i) {
+            return i * 25
+        })
+});
