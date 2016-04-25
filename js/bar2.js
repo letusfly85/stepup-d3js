756049d83b55e3eadb7491d07ca459642a5d0f1a
letusfly85 <letusfly95@gmail.com>
2016-04-25 11:00:46 +0900

    update dataset by clicking

--- js/bar2.js
+++ js/bar2.js
@@ -5,11 +5,23 @@ d3.select("#myGraph")
   .data(dataSet)
   .enter()
   .append("rect")
-  .attr("width", function(d, i){
-    return d + "px";
-  })
   .attr("x", 0)
-  .attr("y", function(d, i) {
-    return i * 25;
+  .attr("y", function(d, i){
+	  return i * 25;
+  })
+  .attr("width", function(d, i) {
+	  return d + "px";
   })
   .attr("height", "20px")
+
+d3.select("#updateButton")
+  .on("click", function() {
+    console.log("enter")
+    dataSet2 = [20, 230, 150, 10, 20];
+    d3.select("#myGraph")
+      .selectAll("rect")
+      .data(dataSet2)
+      .attr("width", function(d, i) {
+        return d + "px";
+      })
+})
