9e8462e2b5c4e88b6f69eed88176060a22991dd2
letusfly85 <letusfly95@gmail.com>
2016-04-25 09:45:51 +0900

    add bar2

---
+++ js/bar2.js
@@ -0,0 +1,15 @@
+var dataSet = [ 300, 130, 5, 60, 240 ];
+
+d3.select("#myGraph")
+  .selectAll("rect")
+  .data(dataSet)
+  .enter()
+  .append("rect")
+  .attr("width", function(d, i){
+    return d + "px";
+  })
+  .attr("x", 0)
+  .attr("y", function(d, i) {
+    return i * 25;
+  })
+  .attr("height", "20px")
