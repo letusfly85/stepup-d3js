b004e5dc3df0987f6e24d760ce6c2799bf8804ee
letusfly85 <letusfly95@gmail.com>
2016-04-25 11:37:11 +0900

    change color on click

--- js/bar2.js
+++ js/bar2.js
@@ -1,31 +1,41 @@
-var dataSet = [ 300, 130, 5, 60, 240 ];
+var dataSet = [300, 130, 5, 60, 240];
 
 d3.select("#myGraph")
-  .selectAll("rect")
-  .data(dataSet)
-  .enter()
-  .append("rect")
-  .attr("x", 0)
-  .attr("y", function(d, i){
-	  return i * 25;
-  })
-  .attr("width", function(d, i) {
-	  return d + "px";
-  })
-  .attr("height", "20px")
+    .selectAll("rect")
+    .data(dataSet)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", function(d, i) {
+        return i * 25;
+    })
+    .on("click", function() {
+        d3.select(this)
+            .style("fill", "cyan")
+    })
+    .attr("height", "20px")
+    .attr("widht", "0px")
+    .transition()
+    .delay(function(d, i) {
+        return i * 500;
+    })
+    .duration(2500)
+    .attr("width", function(d, i) {
+        return d + "px";
+    })
 
 d3.select("#updateButton")
-  .on("click", function() {
-    dataSet2 = [20, 230, 150, 10, 20];
-    for(var i=0; i<dataSet.length; i++) {
-      dataSet2[i] = Math.floor(Math.random() * 320);
-    }
-    console.log("enter")
-    d3.select("#myGraph")
-      .selectAll("rect")
-      .data(dataSet2)
-      .transition()
-      .attr("width", function(d, i) {
-        return d + "px";
-      })
-})
+    .on("click", function() {
+        dataSet2 = [20, 230, 150, 10, 20];
+        for (var i = 0; i < dataSet.length; i++) {
+            dataSet2[i] = Math.floor(Math.random() * 320);
+        }
+        console.log("enter")
+        d3.select("#myGraph")
+            .selectAll("rect")
+            .data(dataSet2)
+            .transition()
+            .attr("width", function(d, i) {
+                return d + "px";
+            })
+    })
